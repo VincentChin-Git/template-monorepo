6f1ac00de3b527e52e6a954526771e2fde418c18
VincentChin-Git <124128510+VincentChin-Git@users.noreply.github.com>
2024-01-08 13:41:37 +0800

    chore: update backend project structure

--- backend/src/config/env/_parsed.ts
+++ backend/src/config/env/_parsed.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
 const { parsed = {} } = dotenv.config({
-  path: path.join(__dirname, `../../../.env.coin`),
+  path: path.join(__dirname, `../../../.env.local`),
 });
 
 export default parsed;
