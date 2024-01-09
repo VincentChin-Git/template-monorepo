78d4f98ba3b4a111e6f19eed31924c4e7dea470f
VincentChin-Git <124128510+VincentChin-Git@users.noreply.github.com>
2024-01-09 11:00:06 +0800

    chore: update

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,11 +1,21 @@
-import EnvConfig from "../config";
+import { useEffect } from "react";
+
+import testApi from "../api/test";
 
 const Index = () => {
-  return (
-    <div>
-      This is Index / Root, do any initalisation here. {EnvConfig.serviceUrl}
-    </div>
-  );
+  const getInfo = async () => {
+    try {
+      const res = await testApi.get({});
+      console.log(res, "success");
+    } catch (error) {
+      console.error(error, "error");
+    }
+  };
+  useEffect(() => {
+    getInfo();
+  }, []);
+
+  return <div>This is Index / Root, do any initalisation here.</div>;
 };
 
 export default Index;
